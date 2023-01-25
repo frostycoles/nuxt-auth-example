f123af3f649a80fd262cc44dc4b59bcf9aad4bd2
frostycoles <dcfrostad@gmail.com>
2023-01-25 16:31:36 -0600

    Adds environment config

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,11 +1,18 @@
 // https://v3.nuxtjs.org/api/configuration/nuxt.config
 export default defineNuxtConfig({
-    css: [
-        '@fortawesome/fontawesome-free/css/all.min.css'
-    ],
-    modules: ['@nuxtjs/tailwindcss', '@sidebase/nuxt-auth'],
-    auth: {
-        origin: process.env.ORIGIN,
-        enableGlobalAppMiddleware: true
-    }
-})
+  css: ['@fortawesome/fontawesome-free/css/all.min.css'],
+  modules: ['@nuxtjs/tailwindcss', '@sidebase/nuxt-auth'],
+  auth: {
+    isEnabled: true,
+    origin: process.env.ORIGIN,
+    basePath: '/api/auth',
+    enableSessionRefreshPeriodically: false,
+    enableSessionRefreshOnWindowFocus: true,
+    enableGlobalAppMiddleware: true,
+  },
+  runtimeConfig: {
+    secret: '',
+    githubClientSecret: '',
+    githubClientId: '',
+  },
+});
